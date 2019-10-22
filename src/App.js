7f3699139aeec812b221796cd5541b8d1a0a8539
Jordi Buj <jordi.buj@jamgo.coop>
2019-10-22 12:44:03 +0200

    QR code generation library

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import QRCode from 'react-qr-code';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <QRCode value="KK" />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Login with DECODE app
         </a>
       </header>
     </div>
